b401e528391c0b7c18f80e49430c3f9049dd9d4e
Nicolas Boutet <amd3002@gmail.com>
2023-02-25 14:21:51 +0200

    Add tests

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -4,6 +4,8 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import requests
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
@@ -27,3 +29,21 @@ def test_elasticsearch_config_file(host, username, groupname, path):
     assert config.is_file
     assert config.user == username
     assert config.group == groupname
+
+
+@pytest.mark.parametrize('name', [
+  ('elasticsearch'),
+])
+def test_service_is_running_and_enabled(host, name):
+    service = host.service(name)
+    assert service.is_enabled
+    assert service.is_running
+
+
+@pytest.mark.parametrize('endpoint,user,password', [
+  ('http://127.0.0.1:9200/_cluster/health', 'elastic', 'changeme'),
+])
+def test_cluster_health(endpoint, user, password):
+    response = requests.get(endpoint, auth=(user, password))
+    data = response.json()
+    assert data['status'] == 'green'
