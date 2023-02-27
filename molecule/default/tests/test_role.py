6f9456c2192a8d68af3a28b14c662008a90ad160
Nicolas Boutet <amd3002@gmail.com>
2023-02-27 09:29:35 +0100

    Add tests (#2)
    * Add tests
    
    * Fix tests
    
    * Add indices test
    
    * Add plugins test
    
    * Add keystore entries test
    
    * Improve tests
    
    * Add templates test
    
    * Add snapshot repositories test
    
    * Install curl before running tests
    
    * Attempt to fix tests

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -4,6 +4,8 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import json
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
@@ -27,3 +29,108 @@ def test_elasticsearch_config_file(host, username, groupname, path):
     assert config.is_file
     assert config.user == username
     assert config.group == groupname
+
+
+@pytest.mark.parametrize('name', [
+  ('elasticsearch'),
+])
+def test_service_is_running_and_enabled(host, name):
+    service = host.service(name)
+    assert service.is_enabled
+    assert service.is_running
+
+
+@pytest.mark.parametrize('user,password', [
+  ('elastic', 'changeme'),
+])
+def test_cluster_health(host, user, password):
+    command = (
+      f"curl -q -u {user}:{password} "
+      f"http://127.0.0.1:9200/_cluster/health"
+    )
+    output = host.run(command)
+    if output.exit_status != 0:
+        pytest.fail('Failed to reach ES API')
+    data = json.loads(output.stdout)
+    assert data['status'] == 'green'
+
+
+@pytest.mark.parametrize('user,password,indice', [
+  ('elastic', 'changeme', 'test-molecule'),
+])
+def test_indices_exist(host, user, password, indice):
+    command = (
+      f"curl -q -u {user}:{password} "
+      f"http://127.0.0.1:9200/_cat/indices/{indice}?format=json"
+    )
+    output = host.run(command)
+    if output.exit_status != 0:
+        pytest.fail('Failed to reach ES API')
+    data = json.loads(output.stdout)
+    assert data[0]['health'] == 'green'
+    assert data[0]['status'] == 'open'
+
+
+@pytest.mark.parametrize('plugin', [
+  ('repository-s3'),
+])
+def test_plugins_exist(host, plugin):
+    output = host.run("/usr/share/elasticsearch/bin/elasticsearch-plugin list")
+    if output.exit_status != 0:
+        pytest.fail('Failed to list ES plugins')
+    assert plugin in output.stdout
+
+
+@pytest.mark.parametrize('entry', [
+  ('s3.client.default.access_key'),
+  ('s3.client.default.secret_key'),
+])
+def test_keystore_entries(host, entry):
+    command = "/usr/share/elasticsearch/bin/elasticsearch-keystore list"
+    output = host.run(command)
+    if output.exit_status != 0:
+        pytest.fail('Failed to list ES keystore entries')
+    assert entry in output.stdout
+
+
+@pytest.mark.parametrize('user,roles', [
+  ('molecule-user-1', 'superuser,custom-role-1'),
+  ('molecule-user-2', 'custom-role-1'),
+])
+def test_users(host, user, roles):
+    command = f"/usr/share/elasticsearch/bin/elasticsearch-users roles {user}"
+    output = host.run(command)
+    if output.exit_status != 0:
+        pytest.fail('Failed to list ES users')
+    assert user in output.stdout
+    assert roles in output.stdout
+
+
+@pytest.mark.parametrize('user,password,template', [
+  ('elastic', 'changeme', 'all-indices'),
+])
+def test_templates_exist(host, user, password, template):
+    command = (
+      f"curl -q -u {user}:{password} "
+      f"http://127.0.0.1:9200/_cat/templates/{template}?format=json"
+    )
+    output = host.run(command)
+    if output.exit_status != 0:
+        pytest.fail('Failed to reach ES API')
+    data = json.loads(output.stdout)
+    assert data[0]['name'] == template
+
+
+@pytest.mark.parametrize('user,password,repository', [
+  ('elastic', 'changeme', 'backup'),
+])
+def test_snapshot_repositories(host, user, password, repository):
+    command = (
+      f"curl -q -u {user}:{password} "
+      f"http://127.0.0.1:9200/_snapshot/{repository}"
+    )
+    output = host.run(command)
+    if output.exit_status != 0:
+        pytest.fail('Failed to reach ES API')
+    data = json.loads(output.stdout)
+    assert repository in data
